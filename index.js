d98525e7b42692afde5690f298cd1de0a524c42a
Nelsondiaz <nelson@bluekite.com>
2014-03-20 15:17:55 -0600

    fixes index

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
 var Hapi = require("hapi");
 var Googleapis = require('googleapis');
 var Jpath = require('json-path');
-var Joi = require('joi');
+ var routes = require('./routes.js');
 
 // Declare internals
 var internals = {};
 
+var serverOptions = {
+    views: {
+        path: 'templates',
+        engines: { html: 'handlebars' },
+        partialsPath: './templates/withPartials',
+        helpersPath: './templates/helpers',
+        isCached: false
+    },
+    cors: true
+};
+
+
 internals.main = function () {
     var server = new Hapi.Server("localhost", 3000);
-    server.on('request', function (request, event, tags) {
-        
-    });
-    
-    server.route({
-	    method: 'GET',
-            path: '/v1/channels/search',
-            config:{
-                description: 'Get a list of channels that match a query string',
-                notes: ['Get a list of channels that match a query string',
-                            'Error status codes',
-                            '200, Ok',
-                            '404, Not Found',
-                            '502, An internal server error occurred'
-                            ],
-                tags: ['api'],
-                handler: function (request, reply) {
-                    reply('hello world');
-                },
-                validate:{
-                    path: Joi.any(),
-                    query: Joi.any()
-                }
-            }
-	});
-
-    var routes = require('./routes.js')(server);
+    server.route(routes.routes);
 
     server.start(function () {
         console.log('tHAPICs Server started at: ' + server.info.uri);
@@ -56,6 +42,6 @@ internals.main = function () {
       // console.log(server.table());
     });
 
-}
+};
 
 internals.main();
