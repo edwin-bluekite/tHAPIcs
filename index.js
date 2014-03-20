27472e3d75df24c142a88dde8ed939298e7e98f7
Edwin Allen <edwin.allen@gmail.com>
2014-03-20 03:30:21 -0600

    Get a list of youtube channels by query string

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Hapi = require("hapi");
+var Googleapis = require('googleapis');
 
 // Declare internals
 var internals = {};
@@ -8,6 +9,18 @@ internals.main = function () {
 
     server.start(function () {
         console.log('tHAPICs Server started at: ' + server.info.uri);
+
+	//Get a youtube channel id
+	Googleapis
+	    .discover('youtube', 'v3')
+	    .execute(
+	    function(err,client){
+		var params = { part: 'snippet',q:"xoom.com",type:"channel", key:"AIzaSyAY2jNk6xt1xiFhajpRc3hBxFD1MlgvV7Y" };
+		var req1 = client.youtube.search.list(params);
+		req1.execute(function (err, response) {
+		    console.log('Channel results', response);
+		    });
+	});
 	//Log the route table if there is one
         console.log(server.table());
     });
